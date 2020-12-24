ebd48600bc0dc2be10b26c73291c1522940584e9
Raquel Pereda <rperedah@gmail.com>
2020-12-24 09:39:21 +0100

    Fist commit of the blog

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require('lodash')
+
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const aboutContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.";
+const contactContent = "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+let posts = [];
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render('home', {startContent: homeStartingContent, posts: posts});
+})
+
+app.get("/about", function(req, res){
+    res.render('about', {aboutContent: aboutContent})
+})
+
+app.get("/contact", function(req, res){
+    res.render('contact', {contactContent: contactContent})
+})
+
+app.get("/compose", function(req, res){
+    res.render("compose")
+})
+
+app.get("/posts/:postName", function(req, res){
+    posts.forEach(element => {
+        if(_.lowerCase(req.params.postName) === _.lowerCase(element.title)){
+            res.render('post', {postTitle: element.title, postContent: element.content})
+        }
+    });
+})
+
+app.post("/compose", function(req, res){
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody
+    }
+    posts.push(post);
+    res.redirect("/");
+})
+
+app.listen(3000, function(){
+    console.log("Server up and running!")
+})
