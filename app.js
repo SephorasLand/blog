bc6d97dd764b4aafedf6cb0ebbe697276d2b81ca
Raquel Pereda <rperedah@gmail.com>
2020-12-30 23:27:30 +0100

    Added the mongoose code to connect to database

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash')
+var _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const aboutContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.";
@@ -10,12 +14,21 @@ let posts = [];
 
 const app = express();
 
+const blogSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model('Post', blogSchema);
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render('home', {startContent: homeStartingContent, posts: posts});
+    Post.find({}, function(err, posts){
+        res.render('home', {startContent: homeStartingContent, posts: posts});
+    })
 })
 
 app.get("/about", function(req, res){
@@ -30,21 +43,28 @@ app.get("/compose", function(req, res){
     res.render("compose")
 })
 
-app.get("/posts/:postName", function(req, res){
-    posts.forEach(element => {
-        if(_.lowerCase(req.params.postName) === _.lowerCase(element.title)){
-            res.render('post', {postTitle: element.title, postContent: element.content})
-        }
-    });
+app.get("/posts/:postId", function(req, res){
+    const requestedPostId = req.params.postId;
+    Post.findOne({_id: requestedPostId}, function(err, post){
+        res.render("post", {
+          title: post.title,
+          content: post.content
+        });
+      });
 })
 
 app.post("/compose", function(req, res){
-    const post = {
+    
+    const post = new Post({
         title: req.body.postTitle,
         content: req.body.postBody
-    }
-    posts.push(post);
-    res.redirect("/");
+    });
+
+    post.save(function(err){
+        if(!err){
+            res.redirect("/");
+        }
+    });
 })
 
 app.listen(3000, function(){
